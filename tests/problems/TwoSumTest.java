f2dea045b3c511431e460aa2e11ee5b156d37944
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-04-11 22:27:55 -0500

    Add TwoSum test cases

--- tests/problems/TwoSumTest.java
+++ tests/problems/TwoSumTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class TwoSumTest {
 
+    //TODO: Find a way to create tests with parametrized inputs like Theories in C#'s xUnit
     @Test
     public void twoSum_givenAnArrayAndTarget_shouldReturnIndexesOfNumbersThatSumToTarget() {
         int[] numbers = {2,7,11,15};
@@ -12,6 +13,20 @@ public class TwoSumTest {
         int[] expected = {0, 1};
 
         int[] result = TwoSum.twoSum(numbers, target);
+        Assert.assertArrayEquals(expected, result);
+
+        numbers = new int[] {3, 2, 4};
+        target = 6;
+        expected = new int[] {1, 2};
+
+        result = TwoSum.twoSum(numbers, target);
+        Assert.assertArrayEquals(expected, result);
+
+        numbers = new int[] {3, 3};
+        target = 6;
+        expected = new int[] {0, 1};
+
+        result = TwoSum.twoSum(numbers, target);
         Assert.assertArrayEquals(expected, result);
     }
 }
