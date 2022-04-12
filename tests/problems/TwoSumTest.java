5dfe995e7092d6bfbd0f6176e80cddd271ec70bf
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-04-11 21:23:27 -0500

    Add unit test for TwoSum problem

---
+++ tests/problems/TwoSumTest.java
@@ -0,0 +1,17 @@
+package problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TwoSumTest {
+
+    @Test
+    public void twoSum_givenAnArrayAndTarget_shouldReturnIndexesOfNumbersThatSumToTarget() {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        int[] expected = {0, 1};
+
+        int[] result = TwoSum.twoSum(numbers, target);
+        Assert.assertArrayEquals(result, expected);
+    }
+}
